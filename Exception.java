4ab9db5fee4fe7d712018cd4de4cf766305f15a4
Thrisha999 <101519219+Thrisha999@users.noreply.github.com>
2023-06-03 07:13:32 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,16 @@
+//Exception
+import java.util.*;
+class Exception
+{
+  public static void main(String[] args)
+  {
+    try
+    {
+      int a=10/0;
+    }
+    catch(ArithmeticException e)
+    {
+      System.out.println("trisha");
+    }
+  }
+}
